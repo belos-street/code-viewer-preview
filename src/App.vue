b089063c57c9f389c3e9c8bb17a2aea5e41a3364
zhi.jiang <zhi.jiang@sectrend.com.cn>
2025-05-13 21:15:17 +0800

    fix: 修改样式

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,19 @@ import demo2 from './example/demo2.vue'
     <demo1 />
     <demo2 />
   </div>
+
+  <svg width="38" height="164" viewBox="0 0 38 164" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M38 24V164L0.25 140V1L38 24" fill="url(#paint0_linear_2073_34671)" stroke="#3EC390"
+      stroke-width="0.506104" />
+    <defs>
+      <linearGradient id="paint0_linear_2073_34671" x1="37.5488" y1="0.999513" x2="89.2191" y2="118.224"
+        gradientUnits="userSpaceOnUse">
+        <stop stop-color="#00FF95" stop-opacity="0.01" />
+        <stop offset="1" stop-color="#00FF95" stop-opacity="0.552941" />
+      </linearGradient>
+    </defs>
+  </svg>
+
 </template>
 
 <style scoped>
