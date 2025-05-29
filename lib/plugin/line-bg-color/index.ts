3f60f0c995c9b442472f2a61de6989631bac6668
belos-street <869135421@qq.com>
2025-05-29 23:51:05 +0800

    fix

--- lib/plugin/line-bg-color/index.ts
+++ lib/plugin/line-bg-color/index.ts
@@ -5,25 +5,7 @@ export const LineBgColorPlugin: Plugin = {
   name: 'line-bg-color',
   _processed: new Set<string | number>(),
 
-  install(context: PluginContext) {
-    // 只处理 visibleItems
-    watch(
-      () => context.visibleItems.value,
-      (visibleItems) => {
-        visibleItems.forEach(line => {
-          if (!line.vNode) {
-            const bgColor = line.meta?.bgColor
-            if (bgColor) {
-              line.vNode = h('div', { style: { background: bgColor } }, line.content)
-            } else {
-              line.vNode = h('div', {}, line.content)
-            }
-          }
-        })
-      },
-      { immediate: true }
-    )
-  },
+  install(context: PluginContext) {},
 
-  uninstall() { }
+  uninstall() {}
 }
